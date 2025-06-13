bdba7c1b7a8b7f36ec99139eaa5e583b5549f6a8
edgd1er <3601462+edgd1er@users.noreply.github.com>
2025-06-13 14:58:10 +0200

    ajout de fonctions pôur le prepare.
    simplification et centralisation des actions.

--- src/app/daily_update_graph.sh
+++ src/app/daily_update_graph.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
 # resend every day, a mean value for each hour then let linky send every minute.
-h=${HTTP_SERVER:-false}
-if [[ ! "nofalse0" =~ ${h,,} ]]; then
-  echo "stopping linky service"
-  supervisorctl stop linky
-  echo "stopping http_plot service"
-  supervisorctl stop http_plot
-  /app/prepare.sh
-fi
-
+/app/prepare.sh -r
--- src/app/prepare.sh
+++ src/app/prepare.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
 h=${HTTP_SERVER:-false}
-if [[ ! "nofalse0" =~ ${h,,} ]]; then
-  echo "starting http_plot service"
-  supervisorctl start http_plot
-  #wait for service to be up
-  sleep 5
-  s=${MYSQL_SEND:-false}
-  if [[ "true1yes" =~ ${s,,} ]]; then
-    echo "Populate graph with data"
-    python3 /app/data_injector.py
-    #let the injector inects before starting linky
-    sleep 5
-  fi
-  echo "end"
-fi
 
-echo "starting linky service"
-supervisorctl start linky
-echo "end"
+#Fonctions
+stop_http_plot() {
+  if [[ ! "nofalse0" =~ ${h,,} ]]; then
+    echo "stopping http_plot service"
+    supervisorctl stop http_plot
+    echo "end"
+  fi
+}
+
+start_http_plot() {
+  if [[ ! "nofalse0" =~ ${h,,} ]]; then
+    echo "starting http_plot service"
+    supervisorctl start http_plot
+    #wait for service to be up
+    sleep 5
+    s=${MYSQL_SEND:-false}
+    if [[ "true1yes" =~ ${s,,} ]]; then
+      echo "Populate graph with data"
+      python3 /app/data_injector.py
+      #let the injector inects before starting linky
+      sleep 5
+    fi
+    echo "end"
+  fi
+}
+
+start_linky() {
+  echo "starting linky service"
+  supervisorctl start linky
+  echo "end"
+}
+
+#Main
+while getopts ":rs" o; do
+    case "${o}" in
+        r) #restart
+            supervisorctl stop linky
+            stop_http_plot
+            start_http_plot
+            start_linky
+            ;;
+        s) #start
+            start_http_plot
+            start_linky
+
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+[[ -z ${*} ]] && /app/prepare.sh -s || true
